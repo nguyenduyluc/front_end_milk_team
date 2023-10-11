655423b266bc54b53b7536a8a064107a74c44a98
MinhHue <haminhhue2001@gmail.com>
2023-10-11 23:51:25 +0700

    demo

--- milk_team_oder/src/main.jsx
+++ milk_team_oder/src/main.jsx
@@ -4,7 +4,8 @@ import ReactDOM from 'react-dom/client'
 // import './index.css'
 
 import './Home.css'
-import Homepage from './Home.jsx'
+// import Homepage from './Home.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
