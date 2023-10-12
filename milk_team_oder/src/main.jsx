dd43a98bc1e4a6b19283f53fa65eac4db9341e0e
MinhHue <haminhhue2001@gmail.com>
2023-10-13 00:25:20 +0700

    test

--- milk_team_oder/src/main.jsx
+++ milk_team_oder/src/main.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import App from './App.jsx'
 // import './index.css'
-
+import './login/login.css'
 import './Home.css'
+import Logout from './login/Logout.jsx'
 // import Homepage from './Home.jsx'
-
+import Login from './login/Login.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Homepage />
+    <Login />
   </React.StrictMode>,
 )
